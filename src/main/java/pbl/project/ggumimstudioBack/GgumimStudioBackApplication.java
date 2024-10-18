4c51c3ecd7300c90aa9aef71dd5195c7c5df997f
ParkHyoJin <bhj00702@gmail.com>
2024-10-19 08:06:55 +0900

    형상관리 시작

---
+++ src/main/java/pbl/project/ggumimstudioBack/GgumimStudioBackApplication.java
@@ -0,0 +1,13 @@
+package pbl.project.ggumimstudioBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgumimStudioBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GgumimStudioBackApplication.class, args);
+    }
+
+}
